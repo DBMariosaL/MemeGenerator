c46cb12f60c588fd83401a403547652e62bea783
DBMariosaL <lorenzo.mariosa@epitech.eu>
2021-03-10 12:18:25 +0100

    rect + js meme genarator

---
+++ react_js_meme_generator/src/App.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState} from 'react';
+import { Meme } from './components/Meme'
+
+const objetToQueryParam = (obj) => {
+  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
+  return '?' + params.join('&');
+}
+
+function App() {
+  const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState(null);
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [meme, setMeme] = useState(null);
+
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+    .then(x => x.json().then(response => setTemplates(response.data.memes)));
+  }, []);
+
+  if (meme) {
+    return (
+    <div style={{ textAlign: "center" }}>
+      <img
+      style={{width: 500}}
+      src={meme} alt="custom meme" />
+      <button type="submit" >Add meme on giphy</button>
+    </div>
+    );
+  }
+
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      {template && (
+      <form onSubmit={async e => {
+        e.preventDefault();
+        const params = {
+          template_id: template.id,
+          text0: topText,
+          text1: bottomText,
+          username: 'xzk03017',
+          password: 'xzk03017@cndps.com',
+        }
+        const response = await fetch(`https://api.imgflip.com/caption_image${objetToQueryParam(params)}`);
+        const json = await response.json();
+        setMeme(json.data.url)
+      }}>
+      <Meme template={template} />
+      <input placeholder = "top text" value={topText} onChange={e => setTopText(e.target.value)} />
+      <input placeholder = "bottom text" value={bottomText} onChange={e => setBottomText(e.target.value)} />
+      <button type="submit" >create memes</button>
+      </form>
+      )}
+      {!template &&
+      (
+        <>
+        <h1>Pick a template</h1>
+        {templates.map(template => {
+        return (
+          <Meme
+          template={template}
+          onClick={() =>{
+            setTemplate (template)
+          }}
+          />
+        )
+      })}</>)
+    }</div>
+  );
+}
+
+export default App;
